9087a0a5ea1d5c643bc2f2f704e0e19a7f08febb
KENTKART\mert.buyuk <mert.buyuk@kentkart.com.tr>
2021-11-11 17:13:17 +0300

    skeleton and unauth network operation class

---
+++ app/src/main/java/com/mb/fooddelivery/utils/DAccesStrategy.kt
@@ -0,0 +1,21 @@
+package com.mb.fooddelivery.utils
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.liveData
+import kotlinx.coroutines.Dispatchers
+
+fun <T> networkOperation(call : suspend() -> Resource<T>) : LiveData<Resource<T>>{
+    return liveData(Dispatchers.IO) {
+        emit(Resource.loading())
+        val networkCall = call.invoke()
+        if (networkCall.status == Resource.Status.SUCCESS){
+            val data = networkCall.data!!
+            emit(Resource.success(data))
+        }
+        else if (networkCall.status == Resource.Status.ERROR){
+            emit(
+                Resource.error("Error ${networkCall.message}")
+            )
+        }
+    }
+}
