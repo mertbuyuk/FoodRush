3c0e6d83a0ab3adc41307d3baa4e1fc0f82a12ec
KENTKART\mert.buyuk <mert.buyuk@kentkart.com.tr>
2021-11-11 17:49:59 +0300

    base data source request

---
+++ app/src/main/java/com/mb/fooddelivery/utils/BaseDataSource.kt
@@ -0,0 +1,21 @@
+package com.mb.fooddelivery.utils
+
+import retrofit2.Response
+
+abstract class BaseDataSource {
+    suspend fun <T> getResult(call : suspend() -> Response<T> ) : Resource<T>{
+        val response  = call()
+        if (response.isSuccessful){
+            val body = response.body()
+            if (body != null){
+                return Resource.success(body)
+            }
+        }
+        val errorBody = response.errorBody().toString()
+        return error("${response.code()} - $errorBody")
+    }
+
+    private fun <T> error(message: String): Resource<T> {
+        return Resource.error("Network error: $message")
+    }
+}
