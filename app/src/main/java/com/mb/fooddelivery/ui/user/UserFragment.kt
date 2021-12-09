26238eb0ec8eb11d946177e8fcc74b76d4056273
DESKTOP-1G1DSBK\kentkart <mert.buyuk@kentkart.com.tr>
2021-12-09 15:30:41 +0300

    user done. room needed

--- app/src/main/java/com/mb/fooddelivery/ui/user/UserFragment.kt
+++ app/src/main/java/com/mb/fooddelivery/ui/user/UserFragment.kt
@@ -6,9 +6,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
+import com.mb.fooddelivery.R
 import com.mb.fooddelivery.databinding.FragmentUserBinding
 import com.mb.fooddelivery.model.data.user.UserBody
+import com.mb.fooddelivery.model.data.user.UserRequest
 import com.mb.fooddelivery.utils.Resource
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -29,7 +34,32 @@ class UserFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         getUser()
+        binding.updateBtn.setOnClickListener {
+            updateUser()
+        }
+    }
 
+    private fun updateUser() {
+        binding.apply {
+            val name = outlinedTextField.editText?.text.toString()
+            val surname = outlinedsurname.editText?.text.toString()
+            val email = outlinedemail.editText?.text.toString()
+            val addres = outlineAdress.editText?.text.toString()
+            val username = outlinedusername.editText?.text.toString()
+            val password = outlinepassword.editText?.text.toString()
+            val phone = outlinePhone.editText?.text.toString()
+            val user = UserRequest(name,surname,email,username,password,addres,phone)
+
+            viewModel.editUserSettings(user).observe(viewLifecycleOwner,{
+
+                when(it.status){
+                    Resource.Status.SUCCESS ->{
+                        Toast.makeText(requireContext(),"Updated!",Toast.LENGTH_SHORT).show()
+                        findNavController().navigate(R.id.global_home)
+                    }
+                }
+            })
+        }
     }
 
     private fun getUser() {
@@ -43,7 +73,13 @@ class UserFragment : Fragment() {
     private fun onSucces(responseBody: UserBody) {
         binding.apply {
             //TODO
-            Log.i("Mert",responseBody.toString())
+            outlinedTextField.editText?.setText(responseBody.name)
+            outlinedsurname.editText?.setText(responseBody.surname)
+            outlinedemail.editText?.setText(responseBody.email)
+            outlinedusername.editText?.setText(responseBody.username)
+            outlinepassword.editText?.setText(responseBody.password)
+            outlineAdress.editText?.setText(responseBody.address)
+            outlinePhone.editText?.setText(responseBody.phone)
         }
     }
 }
--- app/src/main/java/com/mb/fooddelivery/ui/user/UserViewModel.kt
+++ app/src/main/java/com/mb/fooddelivery/ui/user/UserViewModel.kt
@@ -2,6 +2,7 @@ package com.mb.fooddelivery.ui.user
 
 import androidx.lifecycle.ViewModel
 import com.mb.fooddelivery.model.ApiRepository
+import com.mb.fooddelivery.model.data.user.UserRequest
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
@@ -9,4 +10,6 @@ import javax.inject.Inject
 class UserViewModel @Inject constructor(private val apiRepository: ApiRepository) : ViewModel() {
 
     fun getUser() = apiRepository.getUser()
+
+    fun editUserSettings(userRequest: UserRequest) = apiRepository.updateUser(userRequest)
 }
